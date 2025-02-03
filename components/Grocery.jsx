95f7858b0b61aa3145d247ab50918791bf1fa01a
ArchitSr313 <architsr313@gmail.com>
2025-02-03 12:33:19 +0530

    added lazy loading feature

---
+++ components/Grocery.jsx
@@ -0,0 +1,9 @@
+const Grocery = ()=>{
+    return (
+        <div>
+            <h1> This is Grocery Page , It is used for Chunking or Lazy Loading</h1>
+        </div>
+    )
+};
+
+export default Grocery;
--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 // import logo from "./assets/food_app_logo.png";
 // import food_1 from "./assets/food_1.jpg";
@@ -10,6 +10,7 @@ import Contact from "../components/Contact";
 import Cart from "../components/Cart";
 import ResInfo from "../components/ResInfo";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
+// import Grocery from "../components/Grocery";
 
 const AppLayout=()=>{
     return (
@@ -19,6 +20,9 @@ const AppLayout=()=>{
         </div>
     )
 };
+
+//Lazy Loading
+const Grocery = lazy(()=> import("../components/Grocery"));  
 
 const appRouter = createBrowserRouter([
     {
@@ -36,6 +40,10 @@ const appRouter = createBrowserRouter([
             {
                 path:"/contact",
                 element:<Contact/>
+            },
+            {
+                path:"/grocery",
+                element:<Suspense fallback={<h1>Loading......</h1>}><Grocery/></Suspense>
             },
             {
                 path:"/cart",
